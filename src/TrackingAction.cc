d39c0b4c276da58e7509835b4c3ec89fc46ccb90
Jiyoung CHOI <158591410+opercjy@users.noreply.github.com>
2025-08-31 03:05:13 +0900

    Create TrackingAction.cc

---
+++ src/TrackingAction.cc
@@ -0,0 +1,7 @@
+#include "TrackingAction.hh"
+#include "G4Track.hh"
+
+TrackingAction::TrackingAction() : G4UserTrackingAction() {}
+TrackingAction::~TrackingAction() {}
+void TrackingAction::PreUserTrackingAction(const G4Track* /*track*/) {}
+void TrackingAction::PostUserTrackingAction(const G4Track* /*track*/) {}
