071550de6de5bc00b43e3acb8a7f2ee0982e924c
Jiyoung CHOI <158591410+opercjy@users.noreply.github.com>
2025-08-31 15:47:42 +0900

    Update DetectorConstruction.hh

--- include/DetectorConstruction.hh
+++ include/DetectorConstruction.hh
@@ -5,18 +5,19 @@
 #include "globals.hh"
 #include "G4ThreeVector.hh"
 
-// 클래스 전방 선언 (Forward Declaration)
+// 클래스 전방 선언
 class G4VPhysicalVolume;
 class G4LogicalVolume;
 class G4Material;
-class DetectorMessenger; // UI 명령어를 위한 메신저 클래스 전방 선언
+class DetectorMessenger;
 
 /**
  * @class DetectorConstruction
- * @brief 시뮬레이션 환경의 모든 물질과 기하학적 구조를 생성하는 클래스입니다.
+ * @brief 시뮬레이션 환경의 모든 물질과 기하학적 구조를 생성하는 클래스.
  *
- * 두 개의 PMT를 배치하며, DetectorMessenger를 통해 매크로에서
- * 회전 PMT의 각도를 제어하는 기능을 포함합니다.
+ * [수정사항]
+ * 1. 검출기-선원 간 거리(fDetectorDistance)를 멤버 변수로 추가.
+ * 2. 매크로에서 거리와 각도를 모두 제어하는 Setter 함수 제공.
  */
 class DetectorConstruction : public G4VUserDetectorConstruction
 {
@@ -29,12 +30,13 @@ public:
 
   // Messenger에서 호출할 Public Setter 함수
   void SetMovablePMTAngle(G4double angle);
+  void SetDetectorDistance(G4double distance);
 
 private:
   // 내부 헬퍼 함수 선언
   void DefineMaterials();
-  G4LogicalVolume* ConstructDetectorUnit(); // 단일 검출기 유닛 생성 함수 선언
-  G4LogicalVolume* ConstructPMT();        // PMT 생성 함수 선언
+  G4LogicalVolume* ConstructDetectorUnit();
+  G4LogicalVolume* ConstructPMT();
 
   // --- 물질 포인터 ---
   G4Material* fAirMaterial;
@@ -44,9 +46,11 @@ private:
   G4Material* fPmtBodyMaterial;
   G4Material* fSiliconeMaterial;
   G4Material* fVacuumMaterial;
+  G4Material* fEpoxyMaterial; 
 
   // --- 지오메트리 제어용 멤버 변수 ---
   G4double fMovablePMTAngle;
+  G4double fDetectorDistance;
 
   // --- 기타 멤버 변수 ---
   DetectorMessenger* fDetectorMessenger;
